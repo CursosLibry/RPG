3c5198f051b3f290701586673958a37b7ffb3a5c
unknown <gabriel.didomenico@hotmail.com>
2018-11-26 22:55:00 -0200

    Adicionado as páginas de História - GD

---
+++ web/view/user/addHistory.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="zxx">
+   <head>
+      <title>Dragon Hunt a Games Category Bootstrap responsive Website Template | Home :: w3layouts</title>
+      <!--meta tags -->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="keywords" content="Dragon Hunt Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+         Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+      <script>
+         addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+         }, false);
+         
+         function hideURLbar() {
+            window.scrollTo(0, 1);
+         }
+      </script>
+      <!--//meta tags ends here-->
+      <!--booststrap-->
+      <link href="/rpg/web/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+      <!--//booststrap end-->
+      <!-- font-awesome icons -->
+      <link href="/rpg/web/css/fontawesome-all.min.css" rel="stylesheet" type="text/css" media="all">
+      <!-- //font-awesome icons -->
+      <!--stylesheets-->
+      <link href="/rpg/web/css/style.css" rel='stylesheet' type='text/css' media="all">
+      <!--//stylesheets-->
+      <!-- Nav-CSS -->  
+      <script src="/rpg/web/js/jquery-2.2.3.min.js"></script>      
+      <script src="/rpg/web/js/index.js"></script>
+      <link href="/rpg/web/css/nav.css" rel="stylesheet" type="text/css" media="all" />
+      <script src="/rpg/web/js/modernizr.custom.js"></script>
+      <!-- //Nav-CSS -->
+      <link href="/rpg/web/css/timeline.min.css" rel="stylesheet">
+      <link href="//fonts.googleapis.com/css?family=Cinzel+Decorative:400,700" rel="stylesheet">
+      <link href="//fonts.googleapis.com/css?family=Julius+Sans+One" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+   </head>
+   <body>
+         <!--headder-->
+         <section class="contact-inner py-lg-4 py-md-3 py-sm-3 py-3">
+            <div class="container py-lg-5 py-md-5 py-sm-4 py-4">
+             
+               <h3 class="title text-center  mb-lg-5 mb-md-4 mb-sm-4 mb-3"> Crie sua história</h3>
+               <div class=" contact-wls-detail">
+                  <div class="contact-form">
+                     <form action="#" method="post">
+                        <div style="padding: 50px 0px 50px 0px;" class="row agile-wls-contact-mid mb-sm-3 mb-2">
+                           <div  class="col-lg-6 col-md-6 form-group contact-forms">
+                              <input type="text" class="form-control" name="title" placeholder="Titulo" required="">
+                           </div>
+                             <div class="col-lg-6 col-md-6 form-group contact-forms">
+                              <input type="email" class="form-control" name="title" placeholder="Titulo" placeholder="Email" required="">
+                           </div>
+                           </div>
+                           
+                        <button style="background-color: #969696;" type="button" class="btn sent-butnn btn-lg">Com certeza!</button>
+                     </form>
+                     <div style="padding: 50px 50px;" class="row agile-wls-contact-mid mb-sm-3 mb-2">
+                           <div>
+                              Você ainda não existe? Mas isso é um problema <a href="register.php" style="color: black; font-weight: bold;">Clique aqui</a> agora mesmo para invocar-se!
+                           </div>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+                <?php include_once '../../inc/footer.inc.php'; ?>
---
+++ web/view/user/createHistory.php
@@ -0,0 +1,173 @@
+
+<?php include_once '../../inc/header.inc.php'; ?>
+      <!-- banner -->
+      <div class="inner_page-banner">
+      </div>
+      <!--//banner -->
+         <!--headder-->
+         <section class="latest-design py-lg-4 py-md-3 py-sm-3 py-3">
+            <div class="container-fluid py-lg-5 py-md-5 py-sm-4 py-4">
+               <h3 class="title text-center clr mb-lg-5 mb-md-4 mb-sm-4 mb-3">Our History</h3>
+               <div class="timeline">
+                  <div class="timeline__wrap">
+                     <div class="timeline__items">
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>1
+                              </p>
+                              <div style="text-align: center;" class="addPlus"><button type="button">+</button></div>
+                           </div>
+                        </div>
+                  		 <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>2
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>3
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>4
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>5
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>6
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>7
+                              </p>
+                           </div>
+                        </div> <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>8
+                              </p>
+                           </div>
+                        </div>
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>9
+                              </p>
+                           </div>
+                        </div>
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>10
+                              </p>
+                           </div>
+                        </div>
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>11
+                              </p>
+                           </div>
+                        </div>
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>12
+                              </p>
+                           </div>
+                        </div>
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>13
+                              </p>
+                           </div>
+                        </div>
+                        <div  style="display: none;">
+                        <div class="timeline__item">
+                           <div class="timeline__content">
+                              <h6 class="pb-2">2010</h6>
+                              <h4 class="pb-3">Start Game</h4>
+                              <p>14
+                              </p>
+                           </div>
+                        </div>
+                    </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+        
+    	<div id="iframeBolado" class="iframe"><iframe frameborder="1"></iframe><button type="button" id="fechaIframe">X</button></div>
+         <?php include_once '../../inc/footer.inc.php'; ?>
+        <style>
+        iframe{
+		   -webkit-transform: translate(-50%,-50%);
+		   -moz-transform: translate(-50%,-50%);
+		   transform: translate(-50%, -50%);
+		   top: 50%;
+		   left: 50%;
+		   position: fixed;
+		   z-index: 99;
+		   width: 800px;
+		   height: 500px;
+		}
+		.iframe button {
+		  position: absolute;
+		  right: 200px;
+		  top: 80px;
+		  color: red;
+		  z-index: 99999;
+		}
+    	</style>
+        <script>
+        	
+        	$( document ).ready(function(){
+        		$("#iframeBolado").hide();
+        		$(".addPlus").click(function(){
+	        		var pagina = "addHistory.php";
+					var popup = document.querySelector("iframe");
+					popup.src = pagina;
+					$("#iframeBolado").fadeIn("slow", function(){
+						$("#iframeBolado").show();
+						$( document.body ).css("opcaity", "0.1");
+					});
+					
+        		});
+        		$("#fechaIframe").click(function(){
+					$("#iframeBolado").fadeOut("slow", function(){
+						$("#iframeBolado").hide();
+					});
+					
+        		});
+        	});
+
+        </script>
